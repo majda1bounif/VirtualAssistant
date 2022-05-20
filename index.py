b7cea4d27f3cfd6d1574d86fefb06c50d48cc7f7
Majda <majda1bounif@gmail.com>
2022-05-21 01:12:14 +0300

    fixCloseTab

---
+++ index.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask("__name__")
+
+
+@app.route('/')
+def hello():
+    return render_template("index.html")
+
+@app.route("/", methods=["POST", "GET"])
+@app.route("/camera.html")
+def camera():
+    return render_template("camera.html")
+
+
+@app.route("/location.html")
+def location():
+    return render_template("location.html")
+
+
+
+if __name__ == "__main__":
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+    app.run(debug=True)
+
