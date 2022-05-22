f07351cfb10b475c511fdb0419f65403d5d8c8ec
Majda <majda1bounif@gmail.com>
2022-05-22 12:42:56 +0300

    MY GIT

--- index.py
+++ index.py
@@ -1,13 +1,15 @@
+
 from flask import Flask, render_template
+from flask_ngrok import run_with_ngrok
 
 app = Flask("__name__")
-
+run_with_ngrok(app)
 
 @app.route('/')
 def hello():
     return render_template("index.html")
 
-@app.route("/", methods=["POST", "GET"])
+
 @app.route("/camera.html")
 def camera():
     return render_template("camera.html")
@@ -19,8 +21,10 @@ def location():
 
 
 
+
+
 if __name__ == "__main__":
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-    app.run(debug=True)
+    app.run()
 
